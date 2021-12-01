828177c2be24d3d17203a10b2dff15f551643ab1
yotechltd <91316425+yotechltd@users.noreply.github.com>
2021-12-01 18:32:37 +0600

    da

--- user.js
+++ user.js
@@ -2,3 +2,15 @@ name: { type: String, default: "", required: true},
     id: { type: String},
     password: { type: String, required: true},
     city: String,
+        location: {
+        type: {
+          type: String,
+          enum: ['Point'],
+          required: true,
+          default: 'Point'
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
+    },
